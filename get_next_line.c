43ada6973b1ea77c8780fa1cabde90806e17f904
Oleksii Malovichko <omalovic@2-C-8.42heilbronn.de>
2024-12-18 14:14:08 +0100

    commit

--- get_next_line.c
+++ get_next_line.c
@@ -6,111 +6,149 @@
 /*   By: omalovic <omalovic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 14:43:14 by omalovic          #+#    #+#             */
-/*   Updated: 2024/11/09 16:34:22 by omalovic         ###   ########.fr       */
+/*   Updated: 2024/11/15 14:25:11 by omalovic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-void	free_str(char *str, int sign)
+char	*ft_copy_bytes(char *result, char *src1, char *src2, int i2)
 {
-	if (sign)
-		free(str);
-	if (!sign)
-	{
-		if (str != NULL)
-			free(str);
-	}
-}
+	int	i1;
+	int	k;
 
-int	get_i1(char *str)
-{
-	if (str != NULL)
-		return (ft_strlen(str));
-	return (0);
+	i1 = ft_strlen(src1);
+	k = 0;
+	while (k < i1)
+	{
+		result[k] = src1[k];
+		k++;
+	}
+	k = 0;
+	while (k < i2)
+	{
+		result[i1] = src2[k];
+		i1++;
+		k++;
+	}
+	result[i1] = '\0';
+	free(src1);
+	return (result);
 }
 
 char	*gen_str(char *src1, char *src2, int i2)
 {
 	int		i1;
 	char	*result;
-	int		j;
-	int		k;
 
-	i1 = get_i1(src1);
+	if (!src2)
+		return (NULL);
+	if (!src1)
+		return (ft_copystr(&src2, i2, 1));
+	i1 = ft_strlen(src1);
+	if (i1 == -1 || i1 < 0 || i2 < 0)
+		return (NULL);
 	result = (char *)malloc(sizeof(char) * (i1 + i2 + 1));
 	if (!result)
 		return (NULL);
-	j = 0;
-	while (j < i1)
-	{
-		result[j] = src1[j];
-		j++;
-	}
-	k = 0;
-	while (k < i2)
-	{
-		result[j] = src2[k];
-		j++;
-		k++;
-	}
-	result[j] = '\0';
-	free_str(src1, 0);
+	result = ft_copy_bytes(result, src1, src2, i2);
+	if (!result)
+		return (NULL);
 	return (result);
 }
 
-int	checker(char *str)
+char	*get_remainder(char *buffer, int checker_result, int bytes_read)
 {
-	int	i;
+	char	*remainder;
+	int		i;
+	int		size;
 
+	size = bytes_read - checker_result;
+	if (size <= 0)
+		return (NULL);
+	remainder = (char *)malloc(sizeof(char) * (size + 1));
+	if (!remainder)
+		return (NULL);
 	i = 0;
-	while (str[i] != '\0')
+	while (i < size)
 	{
-		if (str[i] == '\n')
-			return (i);
+		remainder[i] = buffer[checker_result + i];
 		i++;
 	}
-	return (i);
+	remainder[size] = '\0';
+	return (remainder);
+}
+
+int	deal_with_remainder(char **remainder, char **result)
+{
+	char	*new_remainder;
+
+	if (*remainder)
+	{
+		if (checker(*remainder) != -1)
+		{
+			*result = gen_str(NULL, *remainder, checker(*remainder));
+			new_remainder = get_remainder(*remainder,
+					checker(*remainder), ft_strlen(*remainder));
+			free(*remainder);
+			*remainder = new_remainder;
+			if (*remainder)
+				return (1);
+			else if (*result)
+				return (1);
+			else
+				return (-1);
+		}
+		*result = ft_copystr(remainder, ft_strlen(*remainder), 1);
+		if (!*result)
+			return (-1);
+		free(*remainder);
+		*remainder = NULL;
+	}
+	return (0);
 }
 
 char	*get_next_line(int fd)
 {
-	ssize_t	bytes_read;
-	char	buffer[BUFFER_SIZE + 1];
-	char	*result = NULL;
-	static char remainder;
+	ssize_t		bytes_read;
+	char		buffer[BUFFER_SIZE + 1];
+	char		*result;
+	static char	*remainder = NULL;
 
+	result = NULL;
+	if (deal_with_remainder(&remainder, &result) == 1)
+		return (result);
+	if (deal_with_remainder(&remainder, &result) == -1)
+		return (NULL);
 	while (1)
 	{
 		bytes_read = read(fd, buffer, BUFFER_SIZE);
 		if (bytes_read == -1)
-		{
-			return (NULL);
-		}
-		if (bytes_read == 0) // срабатывает это условие
-			return (NULL);
+			return (free(remainder), free(result), NULL);
+		if (bytes_read == 0)
+			return (handle_remainder(&result));
 		buffer[bytes_read] = '\0';
-		result = gen_str(result, buffer, checker(buffer));
-		if (!result)
-		{
-			return (NULL);
-		}
-		if (checker(buffer) < (int)bytes_read)
-		{
-			return (result);
-		}
+		if (checker(buffer) != -1)
+			return (process_buffer(result, buffer, bytes_read, &remainder));
+		result = gen_str(result, buffer, bytes_read);
 	}
 	return (NULL);
 }
 
-int main()
+/* int main()
 {
 	int fd = open("/Users/omalovic/get_next_line_project/file", O_RDONLY);
 	if (fd == -1)
 		return 1;
-	printf("%s\n", get_next_line(fd));
-	printf("%s\n", get_next_line(fd));
-	printf("%s\n", get_next_line(fd));
-
-
-}
+	// printf("firstcall:\n%s\nend of first call...\n", get_next_line(fd));
+	// printf("secondcall:\n%s\nend of second call...\n", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+} */
